76587a588b4b0f05a4bff32244d2fb7634b2f874
梁雅洁 <2275164048@qq.com>
2020-10-03 23:09:22 +0800

    build

---
+++ build/precache-manifest.a5f4cf54b0d36dd15fd8cc2fc811617f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584d4771f9f0af211e2734cabbbbb1e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "/static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072982478be226892c0f",
+    "url": "/static/js/main.55832095.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
